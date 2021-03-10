725db78d1b6cb6b440b2faebde6b5ea33bd78ae7
hevar2006 <80356308+hevar2006@users.noreply.github.com>
2021-03-10 12:48:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ client.on('message', message => {
 
 if (!message.guild) return; 
 
-if (message.content === 'p+wara') { 
+if (message.content === '/wara') { 
  if (!message.member.hasPermission("ADMINISTRATOR"))  return;
 if (message.member.voiceChannel) { 
 
@@ -36,7 +36,7 @@ if(message.channel.type === 'dm') {
 
 if (message.content.startsWith("https://discord.gg/")) {
     
-  message.author.send(` دانرا تۆش ڕیکلامەکە بکە https://discord.gg/EXCuvNd`) // لینکی سێرڤەری خۆت لیرە دابنی بە هەتا هەتای بێت
+  message.author.send(` دانرا تۆش ڕیکلامەکە بکە https://discord.gg/YAtKVGE7tK`) // لینکی سێرڤەری خۆت لیرە دابنی بە هەتا هەتای بێت
     
   client.channels.get("755636700482633791").send(
 `> <:ARBELE_UPUP:791411050238836766> By: <@${message.author.id}> ✅ 
@@ -51,10 +51,10 @@ ${message.content}`)
 client.on('ready', () => {
 console.log(`ON ${client.guilds.size} Servers`);
 console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`ڕیکلامەکە بنێرە بۆمن دایدەنێم☑️`) 
+client.user.setGame(`BY | LOST`) 
 });
 
 
 
 
-client.login("");//تۆکین لێرە دانێ
+client.login("ODE3Njg1MzkxNDU2ODYyMjMw.YENG3Q.vSPcQZE6Y1GXpaLGrblW3S2IdTM");//تۆکین لێرە دانێ
